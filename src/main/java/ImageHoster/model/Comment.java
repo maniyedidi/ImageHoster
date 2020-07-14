7829c7995899600da37d12b553b53c41daba9c40
maniyedidi <maniyedidi@gmail.com>
2020-07-14 21:53:35 +0530

    comments functionality

---
+++ src/main/java/ImageHoster/model/Comment.java
@@ -0,0 +1,94 @@
+package ImageHoster.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@Column annotation specifies that the attribute will be mapped to the column in the database.
+    //Here the column name is explicitly mentioned as 'id'
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "createdDate")
+    private Date createdDate;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'comments' table referring the primary key in 'users' table will be 'user_id'
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'images' table referring the primary key in 'users' table will be 'user_id'
+    @JoinColumn(name = "image")
+    private Image image;
+
+    public Comment(){
+
+    }
+    public Comment(String text, Date createdDate, User user, Image image) {
+        super();
+        this.text = text;
+        this.createdDate = createdDate;
+        this.user = user;
+        this.image = image;
+    }
+
+    public Comment(Integer id, String text, Date createdDate, User user, Image image) {
+        super();
+        this.id = id;
+        this.text = text;
+        this.createdDate = createdDate;
+        this.user = user;
+        this.image = image;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+}
